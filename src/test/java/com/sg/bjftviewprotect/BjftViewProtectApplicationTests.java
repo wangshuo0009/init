70553e8e4fef2f4e95a1be1e1ce157aeb0f8b7bf
wangshuo <wangshuo969@163.com>
2024-05-08 21:42:10 +0800

    first commit

---
+++ src/test/java/com/sg/bjftviewprotect/BjftViewProtectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.bjftviewprotect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjftViewProtectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
