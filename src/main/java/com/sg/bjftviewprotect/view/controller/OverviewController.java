3ff234f9b47dd99c192a49677ff8641b3865ddda
wangshuo <wangshuo969@163.com>
2024-05-22 15:49:41 +0800

    区域用户统计，查询改方法，从用户档案查

--- src/main/java/com/sg/bjftviewprotect/view/controller/OverviewController.java
+++ src/main/java/com/sg/bjftviewprotect/view/controller/OverviewController.java
@@ -1,11 +1,14 @@
 package com.sg.bjftviewprotect.view.controller;
 
 import com.sg.bjftviewprotect.system.common.Result;
+import com.sg.bjftviewprotect.system.constant.CommonConstant;
 import com.sg.bjftviewprotect.system.entity.AreaUserCount;
 import com.sg.bjftviewprotect.system.entity.PowerGridView;
+import com.sg.bjftviewprotect.system.entity.PowerUserInfo;
 import com.sg.bjftviewprotect.system.entity.RegionalIntroduction;
 import com.sg.bjftviewprotect.system.service.AreaUserCountService;
 import com.sg.bjftviewprotect.system.service.PowerGridViewService;
+import com.sg.bjftviewprotect.system.service.PowerUserService;
 import com.sg.bjftviewprotect.system.service.RegionalIntroductionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,7 +17,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * <p>
@@ -34,6 +42,8 @@ public class OverviewController {
     private AreaUserCountService areaUserCountService;
     @Autowired
     private PowerGridViewService powerGridViewService;
+    @Autowired
+    private PowerUserService powerUserService;
     @Operation(summary = "北京丰台区域简介查询接口")
     @GetMapping("/regionalIntroduction")
     public Result<?> searchRegionalIntroduction() {
@@ -51,8 +61,36 @@ public class OverviewController {
     @Operation(summary = "区域用户统计查询接口")
     @GetMapping("/areaUserCount")
     public Result<?> areaUserCount() {
-        List<AreaUserCount> list = areaUserCountService.list();
-        return Result.success("查询成功",list);
+        Set<String> highSet = new HashSet<>();
+        Set<String> lowSet = new HashSet<>();
+        List<PowerUserInfo> powerUserInfos = powerUserService.list();
+        for (PowerUserInfo powerUserInfo : powerUserInfos) {
+            if (Objects.equals(powerUserInfo.getType(), CommonConstant.HIGH_VOLTAGE)){
+                // 算法不同，高压用户名会重复，根据名称筛选
+                highSet.add(powerUserInfo.getUsername());
+            } else if (Objects.equals(powerUserInfo.getType(), CommonConstant.LOW_VOLTAGE)){
+                // 算法不同，低压用户名会重复，但是所属楼栋不同根据地址+名称筛选
+                lowSet.add(powerUserInfo.getAddress() + powerUserInfo.getUserCode());
+            }
+        }
+        double allUserNum = highSet.size() + lowSet.size();
+        double highUserNum = highSet.size();
+        double lowUserNum = lowSet.size();
+        List<AreaUserCount> areaUserCounts = areaUserCountService.list();
+        areaUserCounts.forEach(areaUserCount -> {
+            if (areaUserCount.getName().contains("总")){
+                areaUserCount.setNum(areaUserCount.getNum());
+                areaUserCount.setRate(BigDecimal.valueOf(100).setScale(2, RoundingMode.HALF_UP));
+            } else if (areaUserCount.getName().contains("高")){
+                areaUserCount.setNum(areaUserCount.getNum());
+                areaUserCount.setRate(BigDecimal.valueOf((highUserNum / allUserNum) * 100).setScale(2, RoundingMode.HALF_UP));
+            } else if (areaUserCount.getName().contains("低")){
+                areaUserCount.setNum(areaUserCount.getNum());
+                areaUserCount.setRate(BigDecimal.valueOf((lowUserNum / allUserNum) * 100).setScale(2, RoundingMode.HALF_UP));
+
+            }
+        });
+        return Result.success("查询成功",areaUserCounts);
     }
 
     @Operation(summary = "区域用电量统计查询接口")
