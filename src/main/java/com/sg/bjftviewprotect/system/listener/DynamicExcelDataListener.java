64f7e66a319cc3645a7f7681fa61727f6b59e08e
wangshuo <wangshuo969@163.com>
2024-05-22 12:44:41 +0800

    excelUtil工具类

--- src/main/java/com/sg/bjftviewprotect/system/listener/DynamicExcelDataListener.java
+++ src/main/java/com/sg/bjftviewprotect/system/listener/DynamicExcelDataListener.java
@@ -1,4 +1,4 @@
-package com.sg.bjftviewprotect.system.excel;
+package com.sg.bjftviewprotect.system.listener;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
